4199656df7fd506b6925e034ba24279223d62b71
rodrigonevest <56009318+rodrigonevest@users.noreply.github.com>
2021-10-27 20:48:53 -0300

    Interface principal

---
+++ src/CONTROLLER/AutoPecas.java
@@ -0,0 +1,12 @@
+
+ 
+package CONTROLLER;
+
+import VIEW.Principal;
+
+
+public class AutoPecas {
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
+}
