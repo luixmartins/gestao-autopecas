998898abefa116650eab10017f4e456db58e6b71
Luiz Martins <luizmartins.uemg@gmail.com>
2021-12-07 21:03:08 -0300

    update sql

--- src/CONTROLLER/AutoPecas.java
+++ src/CONTROLLER/AutoPecas.java
@@ -1,11 +1,7 @@
 package CONTROLLER;
 
-import DAO.FuncionarioDAO;
-import DAO.UsuarioDAO;
 import VIEW.LoginVIEW;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class AutoPecas {
 
